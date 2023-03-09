0549f230c9c50e71d27e9b32eb4ebbaef018af41
Kohta9521 <kohta0421kochi@gmail.com>
2023-03-09 23:20:16 +0900

    error because fontowesome not use sdn

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,20 @@ import "./Navbar.css";
 import Dropdown from "./Dropdown";
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <>
       <nav className="navbar">
         <Link to="#" className="navbar-logo">
           Mercury
         </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas-times" : "fas fa=bars"}></i>
+          <FontAwesomeIcon icon="fa-solid fa-bars" />
+        </div>
       </nav>
     </>
   );
